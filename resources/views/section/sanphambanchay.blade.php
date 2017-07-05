1e8f9680e0224c3477565d6561e884e6ff81e181
PhilipLam95 <drakaabc456@Gmail.com>
2017-07-05 14:54:04 +0700

    5/7 2h54

--- resources/views/section/sanphambanchay.blade.php
+++ resources/views/section/sanphambanchay.blade.php
@@ -1,5 +1,7 @@
 <!-- san pham ban chay -->
+ 
  <section class="awe-section-3"> 
+
                   <section class="list_style_product ">
                     <div class="new_title_color center">
                       <h2><span></span><a href="https://mendover-theme-1.bizwebvietnam.net/frontpage" title="Sản phẩm bán chạy">Sản phẩm bán chạy</a><span></span></h2>
@@ -18,7 +20,7 @@
                                     <span class="tz-shop-meta">
 
                                       <input name="variantId" value="2782584" type="hidden">
-                                      <a  id="mua_hang" value="{{$product->id}}" class="tzshopping add-cart" title="Mua ngay">
+                                      <a  id="mua_hang" value="{{$product->id}}" class="tzshopping add_to_cart add-cart" title="Mua ngay">
 
                                        <div > Mua ngay </div>
                                       </a>
@@ -79,72 +81,7 @@
                   </section>
                 </section>
 
-   <div id="myModal" class="modal fade" role="dialog" style="display: none;"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close" style="position: relative; z-index: 9;"><span aria-hidden="true">×</span></button><h4 class="modal-title"><span><i class="fa fa-check" aria-hidden="true"></i></span>Thêm vào giỏ hàng thành công</h4></div><div class="modal-body"><div class="media"><div class="media-left"><div class="thumb-1x1"><img width="70px" src="//bizweb.dktcdn.net/thumb/small/100/069/071/products/3.jpg" alt="Căn hộ 3PN  ở Pearl Plaza"></div></div><div class="media-body"><div class="product-title">Căn hộ 3PN  ở Pearl Plaza</div><div class="product-new-price"><span>3.000.000.000 đ</span></div></div></div><button class="btn btn-block btn-outline-red" data-dismiss="modal">Tiếp tục mua hàng</button><a href="/checkout" class="btn btn-block btn-radius">Tiến hành thanh toán »</a></div></div></div></div>
+   
+<div id="myModal" class="modal fade " role="dialog" style="display: none;"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close" style="position: relative; z-index: 9;"><span aria-hidden="true">×</span></button><h4 class="modal-title"><span><i class="fa fa-check" aria-hidden="true"></i></span>Thêm vào giỏ hàng thành công</h4></div><div class="modal-body"><div class="media"><div class="media-left"><div class="thumb-1x1"><img width="70px" src="//bizweb.dktcdn.net/thumb/small/100/069/071/products/3.jpg" alt="Căn hộ 3PN  ở Pearl Plaza"></div></div><div class="media-body"><div class="product-title">Căn hộ 3PN  ở Pearl Plaza</div><div class="product-new-price"><span>3.000.000.000 đ</span></div></div></div><button class="btn btn-block btn-outline-red" data-dismiss="modal">Tiếp tục mua hàng</button><a href="/checkout" class="btn btn-block btn-radius">Tiến hành thanh toán »</a></div></div></div></div>
 
 
-<script type="text/javascript">
-
-$giohang = [];
-$('#mua_hang').bind("click", function(e)
-{
-  var id=$(this).attr('value');
-  var image = $('img#image'+id).attr('src');
-  var name =  $('a#name_sanpham'+id).attr('value');
-  var soluong = 0;
-  var sp = [];
-  sp[id] = id;
-  sp[image] = image;
-  sp[name] = name;
-  sp[sl] = soluong;
-
-
-  //Dom HTMLM Image
-
-  //var route="{{route('buy','id_sp')}}";
-  //route = route.replace('id_sp',id);
-  /*
-  var soluonghang=$('#soluong').attr('soluong');
-      soluonghang=parseInt(soluonghang+$('#id'+id).attr('soluong'))+1;
-
-  var tongtien= $('#price').attr('tongtien');   
-  var gia=$('#gia'+id).attr('giamgia');
-        if(gia==0)
-      {
-        gia=$('#gia'+id).attr('dongia');
-      }
-    tongtien = tongtien + soluonghang*gia;
-       
-
-      $.ajax({
-        url: route,
-        type:'get',
-        data: null,
-        success:function(data){          
-          if(soluonghang>0)
-          {
-
-            document.getElementById('cart-img-photo'+id).innerHTML = "<img src=" + image + "/>";
-            document.getElementById('cart-name'+id).innerHTML = name;
-            document.getElementById('id'+id).innerHTML=soluonghang;
-            document.getElementById('gia'+id).innerHTML=gia;
-
-          }
-            
-          else 
-         {
-
-
-         }
-
-
-
-        }
-
-      })
-  */
-
-
-
- 
-})
-</script>
